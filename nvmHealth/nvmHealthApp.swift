9a131193e554a26bac2c79ac6cebb3a4b38b4938
Nick <nickvmorello@gmail.com>
2024-06-16 14:49:21 -0700

    Initial Commit

---
+++ nvmHealth/nvmHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  nvmHealthApp.swift
+//  nvmHealth
+//
+//  Created by Nick Morello on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct nvmHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
